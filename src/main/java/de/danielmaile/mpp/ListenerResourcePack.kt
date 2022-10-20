3a26ff435db12fb8d490669c32c7ba739edfe83a
Daniel Maile <daniel.maile@netcom-mail.de>
2022-10-20 16:55:23 +0200

    Updated Texture Pack to v3 and Aether Texture Pack to v8

--- src/main/java/de/danielmaile/mpp/ListenerResourcePack.kt
+++ src/main/java/de/danielmaile/mpp/ListenerResourcePack.kt
@@ -35,12 +35,12 @@ class ListenerResourcePack: Listener {
     }
 
     private fun applyDefaultResourcePack(player: Player) {
-        player.setResourcePack("https://github.com/DanielMaile/MPP_ResourcePacks/raw/main/MPP_v3.zip",
+        player.setResourcePack("https://github.com/DanielMaile/MPP_ResourcePacks/raw/main/resourcepacks/MPP_v3.zip",
             "376e954f49daf77e876ac607c90b9beb0dca9a20", true)
     }
 
     private fun applyAetherResourcePack(player: Player) {
-        player.setResourcePack("https://github.com/DanielMaile/MPP_ResourcePacks/raw/main/MPP_Aether_v8.zip",
+        player.setResourcePack("https://github.com/DanielMaile/MPP_ResourcePacks/raw/main/resourcepacks/MPP_Aether_v8.zip",
             "97c290743c14935add7dc9216942b5f0ba34ce32", true)
     }
 }
