880d22699883f09ce7a39a4941b837221a41218f
SexyEis <business.sinar@gmail.com>
2022-12-29 01:14:36 +0100

    fix: added missing Block

--- src/main/kotlin/de/danielmaile/mpp/item/ItemType.kt
+++ src/main/kotlin/de/danielmaile/mpp/item/ItemType.kt
@@ -406,7 +406,8 @@ enum class ItemType(
     HELL_TRIDENT(Material.TRIDENT, ToolAttribute(12.0, 1.0).toAttributeMap(), null),
     HELL_STONE_PICKAXE(Material.STONE_PICKAXE, null, null),
     HELL_STONE_SWORD(Material.STONE_SWORD, ToolAttribute(6.0, 1.0).toAttributeMap(), null),
-    HELL_CROWN(Material.NETHERITE_HELMET, ArmorAttribute(15.0, 1.0, EquipmentSlot.HEAD).toAttributeMap(), null);
+    HELL_CROWN(Material.NETHERITE_HELMET, ArmorAttribute(15.0, 1.0, EquipmentSlot.HEAD).toAttributeMap(), null),
+    AETHER_LEAVES(Material.STONE, null, BlockType.AETHER_LEAVES),;
 
     private val displayName = inst().getLanguageManager().getComponent("items.$name.name")
     private val description = inst().getLanguageManager().getComponentList("items.$name.description")
