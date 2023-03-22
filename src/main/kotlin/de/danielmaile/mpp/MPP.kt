d2c289518e2f8684c3951ca36518c7f3a05081ce
Daniel <daniel@daniel-maile.de>
2023-03-22 18:15:49 +0100

    feat: add support for 1.19.4

--- src/main/kotlin/de/danielmaile/mpp/MPP.kt
+++ src/main/kotlin/de/danielmaile/mpp/MPP.kt
@@ -25,7 +25,6 @@ import de.danielmaile.mpp.data.DataPackManager
 import de.danielmaile.mpp.data.ResourcePackBuilder
 import de.danielmaile.mpp.data.config.ConfigManager
 import de.danielmaile.mpp.data.config.LanguageManager
-import de.danielmaile.mpp.demo.ListenerJoinDemo
 import de.danielmaile.mpp.item.ListenerConverter
 import de.danielmaile.mpp.item.ListenerCrafting
 import de.danielmaile.mpp.item.function.ListenerArmor
@@ -164,7 +163,6 @@ class MPP : JavaPlugin() {
         server.pluginManager.registerEvents(ListenerRift(), this)
         server.pluginManager.registerEvents(ListenerHealer(), this)
         server.pluginManager.registerEvents(ListenerHitman(), this)
-        server.pluginManager.registerEvents(ListenerJoinDemo(), this)
     }
 
     private fun registerRecipes() {
