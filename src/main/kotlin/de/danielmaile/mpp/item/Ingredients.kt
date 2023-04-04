bf5b8ce768b2fc7607369ce296a56ec683bc522d
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2023-04-05 00:29:41 +0200

    feat: Added sticks, zanitite stone and gravitite plate as ingredients

---
+++ src/main/kotlin/de/danielmaile/mpp/item/Ingredients.kt
@@ -0,0 +1,32 @@
+/*
+ * This file is part of MPP.
+ * Copyright (c) 2023 by it's authors. All rights reserved.
+ * MPP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MPP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.danielmaile.mpp.item
+
+import org.bukkit.Material
+
+enum class Ingredients(
+    override val material: Material
+): Item {
+    AETHER_STICK(Material.STICK),
+    ZANITE_STONE(Material.DIAMOND),
+    GRAVITITE_PLATE(Material.NETHERITE_SCRAP);
+
+    override fun register() {
+        ItemRegistry.registerItem("mpp:$name", this)
+    }
+}
