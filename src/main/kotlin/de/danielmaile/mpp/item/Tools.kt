8f8318ea9731c67a48a97cbe47aa4521f90feab9
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2023-04-05 20:14:24 +0200

    feat: Tools now use the same Minecraft item based on their type.

--- src/main/kotlin/de/danielmaile/mpp/item/Tools.kt
+++ src/main/kotlin/de/danielmaile/mpp/item/Tools.kt
@@ -20,25 +20,31 @@ package de.danielmaile.mpp.item
 import org.bukkit.Material
 
 enum class Tools(
-    override val material: Material,
     val toolTier: ToolTier,
     val toolType: ToolType
 ): Item {
-    AETHER_WOODEN_AXE(Material.WOODEN_AXE, ToolTier.AETHER_WOOD, ToolType.AXE),
-    AETHER_WOODEN_HOE(Material.WOODEN_HOE, ToolTier.AETHER_WOOD, ToolType.HOE),
-    AETHER_WOODEN_PICKAXE(Material.WOODEN_PICKAXE, ToolTier.AETHER_WOOD, ToolType.PICKAXE),
-    AETHER_WOODEN_SHOVEL(Material.WOODEN_SHOVEL, ToolTier.AETHER_WOOD, ToolType.SHOVEL),
-    AETHER_STONE_AXE(Material.STONE_AXE, ToolTier.AETHER_STONE, ToolType.AXE),
-    AETHER_STONE_HOE(Material.STONE_HOE, ToolTier.AETHER_STONE, ToolType.HOE),
-    AETHER_STONE_PICKAXE(Material.STONE_PICKAXE, ToolTier.AETHER_STONE, ToolType.PICKAXE),
-    AETHER_STONE_SHOVEL(Material.STONE_SHOVEL, ToolTier.AETHER_STONE, ToolType.SHOVEL),
-    GRAVITITE_AXE(Material.NETHERITE_AXE, ToolTier.GRAVITIE, ToolType.AXE),
-    GRAVITITE_HOE(Material.NETHERITE_HOE, ToolTier.GRAVITIE, ToolType.HOE),
-    GRAVITITE_PICKAXE(Material.NETHERITE_PICKAXE, ToolTier.GRAVITIE, ToolType.PICKAXE),
-    GRAVITITE_SHOVEL(Material.NETHERITE_SHOVEL, ToolTier.GRAVITIE, ToolType.SHOVEL),
-    ZANITE_AXE(Material.DIAMOND_AXE, ToolTier.ZANITE, ToolType.AXE),
-    ZANITE_HOE(Material.DIAMOND_HOE, ToolTier.ZANITE, ToolType.HOE),
-    ZANITE_PICKAXE(Material.DIAMOND_PICKAXE, ToolTier.ZANITE, ToolType.PICKAXE),
-    ZANITE_SHOVEL(Material.DIAMOND_SHOVEL, ToolTier.ZANITE, ToolType.SHOVEL);
+    AETHER_WOODEN_AXE(ToolTier.AETHER_WOOD, ToolType.AXE),
+    AETHER_WOODEN_HOE(ToolTier.AETHER_WOOD, ToolType.HOE),
+    AETHER_WOODEN_PICKAXE(ToolTier.AETHER_WOOD, ToolType.PICKAXE),
+    AETHER_WOODEN_SHOVEL(ToolTier.AETHER_WOOD, ToolType.SHOVEL),
+    AETHER_STONE_AXE(ToolTier.AETHER_STONE, ToolType.AXE),
+    AETHER_STONE_HOE(ToolTier.AETHER_STONE, ToolType.HOE),
+    AETHER_STONE_PICKAXE(ToolTier.AETHER_STONE, ToolType.PICKAXE),
+    AETHER_STONE_SHOVEL(ToolTier.AETHER_STONE, ToolType.SHOVEL),
+    GRAVITITE_AXE(ToolTier.GRAVITIE, ToolType.AXE),
+    GRAVITITE_HOE(ToolTier.GRAVITIE, ToolType.HOE),
+    GRAVITITE_PICKAXE(ToolTier.GRAVITIE, ToolType.PICKAXE),
+    GRAVITITE_SHOVEL(ToolTier.GRAVITIE, ToolType.SHOVEL),
+    ZANITE_AXE(ToolTier.ZANITE, ToolType.AXE),
+    ZANITE_HOE(ToolTier.ZANITE, ToolType.HOE),
+    ZANITE_PICKAXE(ToolTier.ZANITE, ToolType.PICKAXE),
+    ZANITE_SHOVEL(ToolTier.ZANITE, ToolType.SHOVEL);
 
+    override val material: Material
+        get() = when(toolType) {
+            ToolType.AXE -> Material.WOODEN_AXE
+            ToolType.PICKAXE -> Material.WOODEN_PICKAXE
+            ToolType.HOE -> Material.WOODEN_HOE
+            ToolType.SHOVEL -> Material.WOODEN_SHOVEL
+        }
 }
